a8c25dd7867e374c5692a677cb021d9b754b48b6
Esteban Lopez <esdlopezl@gmail.com>
2024-11-21 17:26:20 -0500

    Add main view for clients

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,20 +1,18 @@
 import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
+let db: PrismaClient;
 
 declare global {
-  var __db: PrismaClient | undefined;
+var __db: PrismaClient | undefined;
 }
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-  prisma.$connect();
+db = new PrismaClient();
 } else {
-  if (!global.__db) {
-    global.__db = new PrismaClient();
-    global.__db.$connect();
-  }
-  prisma = global.__db;
+if (!global.__db) {
+global.__db = new PrismaClient();
+}
+db = global.__db;
 }
 
-export { prisma };
+export { db as prisma };
